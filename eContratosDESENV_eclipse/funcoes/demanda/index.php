6f2cc3c8f9cb739b89479420051bbc031c22bcd8
daniel.ribeiro <daniel.ribeiro@SF300451.sefaz.pe.gov.br>
2017-06-16 09:42:26 -0300

    ok

--- eContratosDESENV_eclipse/funcoes/demanda/index.php
+++ eContratosDESENV_eclipse/funcoes/demanda/index.php
@@ -269,8 +269,8 @@ function encaminhar() {
                     <TH class="headertabeladados" width="1%">Origem</TH>
                     <TH class="headertabeladados" width="1%">Atual</TH>
                     <TH class="headertabeladados" width="1%">Tipo</TH>
-                    <TH class="headertabeladados" width="60%">Contrato</TH>
-                    <TH class="headertabeladados"width="30%" nowrap >Texto</TH>
+                    <TH class="headertabeladados" width="40%">Contrato</TH>
+                    <TH class="headertabeladados"width="50%" nowrap >Texto</TH>
                     <TH class="headertabeladados" width="1%">Situação</TH>                    
                     <TH class="headertabeladados" width="1%">Prior.</TH>
                     <TH class="headertabeladados"width="1%" nowrap >Usuário</TH>
