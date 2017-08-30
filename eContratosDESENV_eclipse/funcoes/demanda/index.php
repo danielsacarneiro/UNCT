f0b4f94c70c5eac1c71b9d46e23a51e856cc97f9
daniel.ribeiro <daniel.ribeiro@SF300451.sefaz.pe.gov.br>
2017-08-30 12:47:22 -0300

    ok

--- eContratosDESENV_eclipse/funcoes/demanda/index.php
+++ eContratosDESENV_eclipse/funcoes/demanda/index.php
@@ -234,9 +234,9 @@ function encaminhar() {
 				<TD class="campoformulario" width="1%"><?php echo $comboUsuTramitacao->getHtmlSelect(filtroManterDemanda::$NmAtrCdUsuarioTramitacao,filtroManterDemanda::$NmAtrCdUsuarioTramitacao, $filtro->cdUsuarioTramitacao, true, "camponaoobrigatorio", true);?>
 				<TH class="campoformulario" nowrap width="1%">Doc.Anexo:</TH>
 				<TD class="campoformulario" >
-				Tipo: <?php echo $comboTpDoc->getHtmlSelect(voDocumento::$nmAtrTp,voDocumento::$nmAtrTp, $filtro->tpDocumento, true, "camponaoobrigatorio", true);?>
-				Setor: <?php echo $comboSetor->getHtmlCombo(voDocumento::$nmAtrCdSetor,voDocumento::$nmAtrCdSetor, $filtro->cdSetorDocumento, true, "camponaoobrigatorio", true, "");?> 
+				Tipo: <?php echo $comboTpDoc->getHtmlSelect(voDocumento::$nmAtrTp,voDocumento::$nmAtrTp, $filtro->tpDocumento, true, "camponaoobrigatorio", true);?>				 
 				Número: <INPUT type="text" onkeyup="validarCampoNumericoPositivo(this)" id="<?=voDocumento::$nmAtrSq?>" name="<?=voDocumento::$nmAtrSq?>"  value="<?php echo(complementarCharAEsquerda($filtro->sqDocumento, "0", TAMANHO_CODIGOS));?>"  class="camponaoobrigatorio" size="6" maxlength="5">
+				Setor: <?php echo $comboSetor->getHtmlCombo(voDocumento::$nmAtrCdSetor,voDocumento::$nmAtrCdSetor, $filtro->cdSetorDocumento, true, "camponaoobrigatorio", true, "");?>
 								
 			</TR>
        <?php
